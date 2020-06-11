a9ebbd3baab2122856c57de64b75f370886fc217
yassmeena <zulema11898@gmail.com>
2020-06-11 14:39:21 +0200

    create the add_scripts  function

--- functions.php
+++ functions.php
@@ -4,8 +4,23 @@
 
 */
 function add_styles(){
-    
-    wp_register_style( 'uikit link', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
-    wp_enqueue_style( 'uikit css', 'uikit link');
+     //UIkit CSS 
+    wp_register_style( 'uikitLink', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
+    wp_enqueue_style( 'uikitCSS', 'uikitLink');
+    //fontawesome css
+    wp_register_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.8.1/css/all.css' );
+    wp_enqueue_style( 'fontawesome css', 'fontawesome');
+}
+/*
+ function to add my styles sheets
+
+*/
+function add_scripts(){
+    //UIkit js 
+    wp_register_script( 'uikitJ', 'https://cdn.jsdelivr.net/npm/uikit@3.3.0/dist/js/uikit.min.js' );
+    wp_register_script( 'uikitJS ', 'uikitJ',array(),false,true);
+    //UIkit Icons
+    wp_register_script( 'uikitI', 'https://cdn.jsdelivr.net/npm/uikit@3.3.0/dist/js/uikit-icons.min.js' );
+    wp_register_script( 'uikitIcons', 'uikitI',array(),false,true);
 }
 ?>
