d24247d450bc900f3d9dc6edc6c32ba4f3c518da
yassmeena <zulema11898@gmail.com>
2020-06-14 15:09:32 +0200

    start to get the posts from wordpress dashboard

--- functions.php
+++ functions.php
@@ -41,19 +41,9 @@ function Primary_nav_menu(){
     ));
 }
 
-//function to add image on nav 
-add_filter( 'menu_image_default_sizes', function($sizes){
-  
-  // remove the default 36x36 size
-  unset($sizes['menu-36x36']);
-  
-  // add a new size
-  $sizes['menu-50x50'] = array(50,50);
-  
-  // return $sizes (required)
-  return $sizes;
-  
-});
+//add featured image support
+add_theme_support( 'post-thumbnails' );
+
 
 /*
 add_action() finction
--- index.php
+++ index.php
@@ -1,5 +1,44 @@
 <!-- GET header -->
 <?php get_header(); ?>
 
+
+<!--START .section__type-hero-->
+    <section class="section__type-hero uk-contianer">
+     <?php $home_catquery = new WP_Query( 'cat=home&posts_per_page=5' ); ?> 
+     <!-- check if we have posts  -->
+    <?php  if ( $home_catquery->have_posts() ): ?> 
+    <?php while($home_catquery->have_posts()) : $home_catquery->the_post(); ?>
+     <!-- START .hero__background  -->
+   <div class="uk-grid-column-small uk-grid-row-large uk-child-width-1-2@s uk-text-center" uk-grid>
+   
+        <!-- START .hero__type-content -->
+        <div class='hero__type-content'>
+          <h1><?php the_title(); ?></h1>
+   
+
+        </div>
+        <!-- END .hero__type-content -->
+        
+        <!-- START .hero__type-img -->
+        <div class='hero__type-img'>
+         <img src="http://localhost:8000/wp-content/uploads/2020/06/working.png">
+        </div>
+        <!-- END .hero__type-img -->
+       
+            
+        
+</div>
+</div>
+ <?php  
+ //END wordpress loop
+             endwhile;
+             //END if condition
+             endif; 
+                wp_reset_postdata();
+            ?>
+
+    </section>
+    <!--END .section__type-hero-->
+
 <!-- GET footer -->
 <?php get_footer(); ?>
